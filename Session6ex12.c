2bcc31b978fb0505cc41e96fe19a400b33862a10
hluong2702 <hoangluong2702@gmail.com>
2024-11-27 14:21:10 +0700

    Update Session6ex12.c

--- Session6ex12.c
+++ Session6ex12.c
@@ -9,21 +9,20 @@ int main(){
 
     int a = 0, b= 1, next;
 
-    printf("Day Fibonacci gom %d so dau tien:\n",n);
+    printf("Day Fibonacci %d so dau tien: ",n);
 
     for (int i=1;i<=n;i++){
         if(i ==1){
-            printf("%d",a);
+            printf("%d ",a);
         }else if( i ==2){
-            printf("%d",b);
+            printf("%d ",b);
         }else{
             next = a + b;
-            printf("%d",next);
-            a=b;
-            b=next;
+            printf("%d ",next); 
+            a= b;
+            b= next;
         }
     }
-    printf("\n");
 
     return 0;
 }
